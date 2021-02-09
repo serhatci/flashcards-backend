d6f0923c6f85a97039f59e3072c196c6c82351f2
serhat <serhatciftci@gmail.com>
2021-02-09 13:00:17 +0100

    Refactor get all method

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -22,37 +22,51 @@ class Database():
 
     @staticmethod
     def get_all(uid):
+        """provives all data belong to specific user"""
         user_topics = Database.get_topics(uid)
-        user_flashcards = {}
-        for topic in user_topics:
-            title = Database.camel_case(topic['title'])
-            words = Database.get_words(title, topic['flashcards'])
-            user_flashcards[title] = words
-        all = {"titles":
-               [
-                   [i["title"],
-                    Database.camel_case(i["title"])]
-                   for i in user_topics
-               ]}
-        all = {**all, **user_flashcards}
+        user_flashcards = Database.get_flashcards(user_topics)
+        all = Database.combine(user_topics, user_flashcards)
         return jsonify(all)
 
     @staticmethod
     def get_topics(uid):
+        """provides all topics that belong to user"""
         conn = Database.mongo.flashcards.users
         topics = list(conn.find(
             {'id': uid}, {'_id': 0, 'topics': 1}))[0]['topics']
         return topics
 
     @staticmethod
+    def get_flashcards(user_topics):
+        """provides all flashcards from DB that belongs to user"""
+        flashcards = {}
+        for topic in user_topics:
+            title = Database.camel_case(topic['title'])
+            words = Database.get_words(title, topic['flashcards'])
+            flashcards[title] = words
+        return flashcards
+
+    @staticmethod
     def get_words(collection, words_id):
+        """provides all words from specific collection"""
         conn = Database.mongo.flashcards[collection]
         words = list(conn.find(
             {'_id': {'$in': words_id}}, {'_id': 0}))
         return words
 
-    # converts titels to camel case as collection name
+    @staticmethod
+    def combine(user_topics, user_flashcards):
+        "combines topics and flashcards to a single dict"
+        titles = {"titles":
+                  [
+                      {"str": i["title"],
+                       "camelCase":Database.camel_case(i["title"])}
+                      for i in user_topics
+                  ]}
+        return {**titles, **user_flashcards}
+
     @staticmethod
     def camel_case(word):
+        """converts words or sentences to camelCase"""
         snake = ''.join(x.capitalize() for x in word.split(' '))
         return (snake[0].lower() + snake[1:])
