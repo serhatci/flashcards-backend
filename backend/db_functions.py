d1c35df59557b08f428e155385052013a7e123a0
serhat <serhatciftci@gmail.com>
2021-02-27 20:30:28 +0100

    Add create topics func

--- backend/db_functions.py
+++ backend/db_functions.py
@@ -2,7 +2,8 @@
 
 
 def combine(user_info, user_flashcards):
-    "combines username, user topics and flashcards to a single dict"
+    """Combines username, user topics and flashcards to a single dict
+    """
     username = {"username": user_info['username']}
     titles = {"titles":
               [
@@ -14,6 +15,18 @@ def combine(user_info, user_flashcards):
 
 
 def camel_case(word):
-    """converts words or sentences to camelCase"""
+    """Converts words or sentences to camelCase
+    """
     snake = ''.join(x.capitalize() for x in word.split(' '))
     return (snake[0].lower() + snake[1:])
+
+
+def create_topics(existing_topics, new_topics):
+    """Creates new topics of user for updating DB"""
+    remaining = [item for item in existing_topics if item['title']
+                 in new_topics]
+    new_titles = list(
+        set(new_topics) - set([topic['title'] for topic in remaining]))
+    new_topics = [{'title': new_topic, 'flashcards': []}
+                  for new_topic in new_titles]
+    return new_topics + remaining
