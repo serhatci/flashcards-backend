e1fe21bd003e67a12e287db6a56ab1649fa6ee59
serhat <serhatciftci@gmail.com>
2021-02-27 15:42:02 +0100

    Add custom error catching to routes

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort, make_response, jsonify
 from backend.db_methods import Database as db
 from flask_cors import CORS
 import os
@@ -10,30 +10,64 @@ cors = CORS(api, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @api.route('/test')
 def test():
-    """Tests Mongo DB connection"""
-    return f'''<pre>Flask Working correctly</pre>
-<pre>{db.test_connection()}</pre>'''
+    """Tests Mongo DB connection
+    """
+    try:
+        return f'''<pre>Flask Working correctly</pre>
+                <pre>{db.test_connection()}</pre>'''
+    except:
+        msg = "Error occurred in DB connection!"
+        abort(make_response(jsonify(message=msg), 404))
 
 
 @api.route('/', methods=['GET'])
-def user_flashcards():
-    """Provides all data belong to the user including guest"""
-    uid = request.headers.get('Authentication')
-    if uid == "guest":
-        uid = os.environ.get('MASTER')
-    return db.get_all(uid)
+def get_user_flashcards():
+    """Provides all data belong to the user including guest
+    """
+    try:
+        uid = request.headers.get('Authentication')
+        if uid == "guest":
+            uid = os.environ.get('MASTER')
+        return db.get_all(uid)
+    except:
+        msg = "Data could not be collected from DB!"
+        abort(make_response(jsonify(message=msg), 404))
+
+
+@api.route('/update-data', methods=['PUT'])
+def update_user_flashcards_data():
+    """Updates given user data to new flashcards data
+    """
+    try:
+        new_data = request.json
+        if (new_data["topics"]):
+            return db.update_user_topics(new_data)
+        return db.update_user_flashcards(new_data)
+    except:
+        msg = "User data could not be updated!"
+        abort(make_response(jsonify(message=msg), 404))
 
 
 @api.route('/add-user', methods=['POST'])
 def add_user():
-    """Adds new user to the DB"""
-    user_data = request.json
-    master_user = os.environ.get('MASTER')
-    return db.add_user(user_data, master_user)
+    """Adds new user to the DB
+    """
+    try:
+        user_data = request.json
+        master_user = os.environ.get('MASTER')
+        return db.add_user(user_data, master_user)
+    except:
+        msg = "User could not be added to DB!"
+        abort(make_response(jsonify(message=msg), 404))
 
 
 @api.route('/delete-user', methods=['POST'])
 def delete_user():
-    """Delete given user from the DB"""
-    user_data = request.json
-    return db.delete_user(user_data)
+    """Delete given user from the DB
+    """
+    try:
+        user_data = request.json
+        return db.delete_user(user_data)
+    except:
+        msg = "User could not be deleted from DB!"
+        abort(make_response(jsonify(message=msg), 404))
