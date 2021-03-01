4cec18b1888ca8d78f7f0d1b69a991cc6cdebf52
serhat <serhatciftci@gmail.com>
2021-03-01 17:48:46 +0100

    BREAKING CHANGE: functions are re-written

--- backend/db_functions.py
+++ backend/db_functions.py
@@ -1,16 +1,35 @@
 """Provides functions for Database methods"""
 
 
-def organize_user_data(user_data):
-    """Organize user data suitable for front end request
+def frontend_user_data(user_data):
+    """Convert user data to suitable format for frontend
     """
     titles = [{'str': item['title'], 'camelCase': camel_case(
         item['title'])} for item in user_data['topics']]
-    flashcards = [{item['title']: [item['flashcards']]}
-                  for item in user_data['topics']]
+    flashcards = {camel_case(item['title']): item['flashcards']
+                  for item in user_data['topics']}
     return {'username': user_data['userName'],
             'titles': titles,
             'flashcards': flashcards}
+
+
+def backend_user_data(user_data):
+    """Convert user data to suitable format for backend
+    """
+    # userID: currentUser.uid,
+    # flashcard_titles: titles,
+    # cards: flashcards,
+
+    def matching_cards(title):
+        try:
+            return user_data['cards'][title]
+        except KeyError:
+            return []
+
+    topics = [{'title': title['str'],
+               'flashcards': matching_cards(title['camelCase'])}
+              for title in user_data['flashcard_titles']]
+    return user_data['userID'], topics
 
 
 def camel_case(word):
@@ -18,14 +37,3 @@ def camel_case(word):
     """
     snake = ''.join(x.capitalize() for x in word.split(' '))
     return (snake[0].lower() + snake[1:])
-
-
-def compare_topics(existing_topics, new_topics):
-    """Creates new topics of user for updating DB"""
-    remaining = [item for item in existing_topics if item['title']
-                 in new_topics]
-    new_titles = list(
-        set(new_topics) - set([topic['title'] for topic in remaining]))
-    new_topics = [{'title': new_topic, 'flashcards': []}
-                  for new_topic in new_titles]
-    return new_topics + remaining
