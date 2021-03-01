d4059b173b5ad6675e61aafe8d3a9f4f92d1c91d
serhat <serhatciftci@gmail.com>
2021-03-01 17:47:58 +0100

    BREAKING CHANGE: methods are re-written

--- backend/db_methods.py
+++ backend/db_methods.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from flask import jsonify, abort, make_response, Response
-from .db_functions import organize_user_data, camel_case, compare_topics
+from datetime import datetime
 import os
 
 
@@ -23,8 +23,9 @@ class Database():
         """
         try:
             return Database.mongo.server_info()
-        except Exception as err:
-            print(err)
+        except:
+            msg = 'Connection is failed to DB!'
+            return abort(make_response(jsonify(message=msg), 404))
 
     @staticmethod
     def get_all_user_data(uid):
@@ -33,11 +34,68 @@ class Database():
         try:
             conn = Database.mongo.germanFlashcards.users
             result = list(conn.find({'userID': uid}, {
-                '_id': 0, 'creation_date': 0, 'userID': 0}))[0]
+                '_id': 0, 'creation_date': 0, 'userID': 0, 'email': 0}))[0]
             if result != []:
-                return jsonify(organize_user_data(result))
+                return result
             else:
-                raise Exception("no user")
+                raise Exception("No user in DB!")
         except:
             msg = 'User data could not be collected from DB!'
+            return abort(make_response(jsonify(message=msg), 404))
+
+    @staticmethod
+    def update_user_data(uid, new_topics):
+        """Updates all data belong to specific user
+        """
+        try:
+            conn = Database.mongo.germanFlashcards.users
+            conn.update({'userID': uid}, {'$set': {'topics': new_topics}})
+            return Response(status=200)
+        except:
+            msg = 'User data could not be updated on DB!'
+            return abort(make_response(jsonify(message=msg), 404))
+
+    @staticmethod
+    def user_not_exists(uid):
+        """Checks if user  existed in DB or not?
+        """
+        conn = Database.mongo.germanFlashcards.users
+        result = list(conn.find({'userID': uid}))
+        return True if result == [] else False
+
+    @staticmethod
+    def add_new_user(user):
+        """Insert new user to users collection
+        """
+        master_uid = os.environ.get('MASTER')
+        now = datetime.now()
+        try:
+            core_topics = Database.get_all_user_data(master_uid)['topics']
+            new_user = {'creation_date': now,
+                        'userID': user['userID'],
+                        'userName': user['username'],
+                        'email': user['email'],
+                        'topics': core_topics}
+
+            conn = Database.mongo.germanFlashcards.users
+            conn.insert(new_user)
+            return Response(status=200)
+        except:
+            msg = 'User data could not be added to DB!'
+            return abort(make_response(jsonify(message=msg), 404))
+
+    @ staticmethod
+    def delete_user(user):
+        """Delete given user from users collection
+        """
+        if user['userID'] == os.environ.get('MASTER'):
+            msg = "Master user can not be deleted"
+            return abort(make_response(jsonify(message=msg), 404))
+
+        try:
+            conn = Database.mongo.germanFlashcards.users
+            conn.delete_one({'userID': user['userID']})
+            return Response(status=200)
+        except:
+            msg = "User could not be deleted from DB!"
             return abort(make_response(jsonify(message=msg), 404))
