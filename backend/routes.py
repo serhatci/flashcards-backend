9d836c1fa8ce5d81cc234d5a3a8253ed7bf3bf8c
serhat <serhatciftci@gmail.com>
2021-03-01 17:44:38 +0100

    Remove try-except blocks & correct spelling

--- backend/routes.py
+++ backend/routes.py
@@ -1,73 +1,54 @@
-from flask import Blueprint, request, abort, make_response, jsonify
+from flask import Blueprint, request, jsonify
+from .db_functions import frontend_user_data, backend_user_data
 from backend.db_methods import Database as db
 from flask_cors import CORS
 import os
 
 
 api = Blueprint('api', __name__, url_prefix='/api')
-cors = CORS(api, resources={r"/*": {"origins": "http://localhost:3000"}})
+cors = CORS(api, resources={r'/*': {'origins': 'http://localhost:3000'}})
 
 
 @api.route('/test')
 def test():
-    '''Tests Mongo DB connection
-    '''
-    try:
-        return f'''<pre>Flask Working correctly</pre>
-                <pre>{db.test_connection()}</pre>'''
-    except:
-        msg = 'Error occurred in DB connection!'
-        abort(make_response(jsonify(message=msg), 404))
+    """Tests Mongo DB connection
+    """
+    return f"""<pre>Flask Working correctly</pre>
+    <pre>{db.test_connection()}</pre>"""
 
 
 @api.route('/', methods=['GET'])
-def get_all_user_data():
-    '''Provides all data belong to the user including guest
-    '''
+def get_all_data():
+    """Provides all data belong to the user including guest
+    """
     uid = request.headers.get('Authentication')
-    # if user is guest, master user data will be sent
-    if uid == 'guest':
+    # if user is guest or not exist, master user data will be sent
+    if (uid == 'guest' or db.user_not_exists(uid)):
         uid = os.environ.get('MASTER')
-    return db.get_all_user_data(uid)
+    result = db.get_all_user_data(uid)
+    return jsonify(frontend_user_data(result))
 
 
-@api.route('/update-data', methods=['PUT'])
+@ api.route('/update-data', methods=['PUT'])
 def update_user_flashcards_data():
-    '''Updates given user data to new flashcards data
-    '''
-    try:
-        new_data = request.json
-        if (new_data['topics'] or new_data['topics'] == []):
-            return db.update_user_topics(new_data)
-        elif (new_data['cards'] or new_data['cards'] == []):
-            return db.update_flashcards(new_data)
-    except:
-        msg = 'User data could not be updated!'
-        abort(make_response(jsonify(message=msg), 404))
+    """Updates data of given user data on DB
+    """
+    new_data = request.json
+    uid, new_topics = backend_user_data(new_data)
+    return db.update_user_data(uid, new_topics)
 
 
 @ api.route('/add-user', methods=['POST'])
 def add_user():
-    '''Adds new user to the DB
-    '''
-    try:
-        user_data = request.json
-        return db.add_user(user_data)
-    except:
-        msg = 'User could not be added to DB!'
-        abort(make_response(jsonify(message=msg), 404))
+    """Adds new user to the DB
+    """
+    new_user = request.json
+    return db.add_new_user(new_user)
 
 
 @ api.route('/delete-user', methods=['POST'])
 def delete_user():
-    '''Delete given user from the DB
-    '''
-
-    try:
-        user_data = request.json
-        if user_data['userID'] == os.environ.get('MASTER'):
-            raise Exception("Master user can not be deleted")
-        return db.delete_user(user_data)
-    except:
-        msg = 'User could not be deleted from DB!'
-        abort(make_response(jsonify(message=msg), 404))
+    """Delete given user from the DB
+    """
+    user = request.json
+    return db.delete_user(user)
