b04b11f60265a5346770d6071f252817ad5ebfc0
serhat <serhatciftci@gmail.com>
2021-03-02 20:43:10 +0100

    Remove create func

--- run.py
+++ run.py
@@ -1,29 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask,
 from backend.db_methods import Database
 from backend.settings import DevelopmentConfig, ProductionConfig, TestingConfig
 from backend.routes import api
 
+"""Starts flask app
+"""
+app = Flask(__name__)
 
-def create_app():
-    """Starts flask app
-    """
-    app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False  # ads UTF-8 support
 
-    app.config['JSON_AS_ASCII'] = False  # ads UTF-8 support
+if app.config['ENV'] == 'production':
+    app.config.from_object(ProductionConfig)
+elif app.config['ENV'] == 'testing':
+    app.config.from_object(TestingConfig)
+else:
+    app.config.from_object(DevelopmentConfig)
 
-    if app.config['ENV'] == 'production':
-        app.config.from_object(ProductionConfig)
-    elif app.config['ENV'] == 'testing':
-        app.config.from_object(TestingConfig)
-    else:
-        app.config.from_object(DevelopmentConfig)
+app.register_blueprint(api)
 
-    app.register_blueprint(api)
-
-    Database(app)  # creates database connection
-
-    return app
+Database(app)  # creates database connection
 
 
 if __name__ == "__main__":
-    create_app()
+    app.run()
